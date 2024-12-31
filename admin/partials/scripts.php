d49e950293fc5e108a73f43f9a9c3aab3320e7f7
hasan <hasan11@gmail.com>
2025-01-01 01:07:19 +0300

    حفظ التغييرات الحالية

---
+++ admin/partials/scripts.php
@@ -0,0 +1,15 @@
+ <!-- CORE PLUGINS-->
+ <script src="../css/assets/vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="../css/assets/vendors/popper.js/dist/umd/popper.min.js" type="text/javascript"></script>
+    <script src="../css/assets/vendors/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../css/assets/vendors/metisMenu/dist/metisMenu.min.js" type="text/javascript"></script>
+    <script src="../css/assets/vendors/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL PLUGINS-->
+    <script src="../css/assets/vendors/chart.js/dist/Chart.min.js" type="text/javascript"></script>
+    <script src="../css/assets/vendors/jvectormap/jquery-jvectormap-2.0.3.min.js" type="text/javascript"></script>
+    <script src="../css/assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>
+    <script src="../css/assets/vendors/jvectormap/jquery-jvectormap-us-aea-en.js" type="text/javascript"></script>
+    <!-- CORE SCRIPTS-->
+    <script src="../css/assets/js/app.min.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL SCRIPTS-->
+    <script src="../css/assets/js/scripts/dashboard_1_demo.js" type="text/javascript"></script>
---
+++ admin/partials/sidebar.php
@@ -0,0 +1,21 @@
+<nav class="page-sidebar" id="sidebar">
+            <div id="sidebar-collapse">
+                <div class="admin-block d-flex">
+                    <div>
+                        <img src="../css/assets/img/admin-avatar.png" width="45px" />
+                    </div>
+                    <div class="admin-info">
+                        <div class="font-strong">James Brown</div><small>Administrator</small></div>
+                </div>
+                <ul class="side-menu metismenu">
+                    <li>
+                        <a class="active" href="./dashboard.php"><i class="sidebar-item-icon fa fa-th-large"></i>
+                            <span class="nav-label">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="heading">FEATURES</li>
+                   
+                    <li class="heading">PAGES</li>
+                   </ul>
+            </div>
+        </nav>
