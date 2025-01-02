00f01a3b15a1733896e1a091739b5aba438f8bc9
Karam <karammaarouf@gmail.com>
2025-01-02 17:31:36 +0300

    تعديل بعد خربان الادمن

--- admin/partials/sidebar.php
+++ admin/partials/sidebar.php
@@ -7,19 +7,14 @@
                         <img src="../css/assets/img/admin-avatar.png" width="45px" />
                     </div>
                     <div class="admin-info">
-<<<<<<< HEAD
-                        <div class="font-strong">ماعرفت جيب الاسم</div><small><?php echo $_SESSION["type"]; ?></small></div>
-=======
+
                         <div class="font-strong"><?= $_SESSION['user_name'] ?></div><small>Administrator</small></div>
->>>>>>> bc903e7fa18b97abfca9b60f8d1b446fa60d78d8
+
                 </div>
                 <ul class="side-menu metismenu">
                     <li>
                         <a class="active" href="./dashboard.php"><i class="sidebar-item-icon fa fa-th-large"></i>
                             <span class="nav-label">Dashboard</span>
-                        </a>
-                        <a class="active" href="../index.php"><i class="sidebar-item-icon fa fa-home"></i>
-                            <span class="nav-label">index</span>
                         </a>
                         
                     </li>
