7971068dc60db46db2855bc95e14477732bc3161
hasan <hasan11@gmail.com>
2025-01-02 17:34:08 +0300

    تعديل اليوزر

---
+++ user/partials/head.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>profile</title>
+    <!-- GLOBAL MAINLY STYLES-->
+    <link href="../css/assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../css/assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="../css/assets/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
+    <!-- PLUGINS STYLES-->
+    <link href="../css/assets/vendors/DataTables/datatables.min.css" rel="stylesheet" />
+    <!-- PLUGINS STYLES-->
+    <link href="../css/assets/vendors/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
+    <!-- THEME STYLES-->
+    <link href="../css/assets/css/main.min.css" rel="stylesheet" />
+    <!-- PAGE LEVEL STYLES-->
+</head>
---
+++ user/partials/sidebar.php
@@ -0,0 +1,36 @@
+
+
+<nav class="page-sidebar" id="sidebar">
+            <div id="sidebar-collapse">
+                <div class="admin-block d-flex">
+                    <div>
+                        <img src="../css/assets/img/admin-avatar.png" width="45px" />
+                    </div>
+                    <div class="admin-info">
+
+                        <div class="font-strong"><?= $_SESSION['user_name'] ?></div><small>Administrator</small></div>
+
+                </div>
+                <ul class="side-menu metismenu">
+                    <li>
+                        <a class="active" href="./dashboard.php"><i class="sidebar-item-icon fa fa-th-large"></i>
+                            <span class="nav-label">Dashboard</span>
+                        </a>
+                        
+                    </li>
+                    <li class="heading">FEATURES</li>
+                    <li>
+                        <a href="javascript:;"><i class="sidebar-item-icon fa fa-file-text"></i>
+                            <span class="nav-label">Complaints</span><i class="fa fa-angle-left arrow"></i>
+                        </a>
+                        <ul class="nav-2-level collapse">
+                            <li>
+                                <a href="./complaints.php">View Complaints</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="heading">PAGES</li>
+                   </ul>
+            </div>
+        </nav>
